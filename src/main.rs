0bf2fac152548605c1761a53e0d4fbc291f10d1f
roman.lex <lexr@edu.htl-villach.at>
2024-12-02 20:49:37 +0100

    day1_1 angefange

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello World");
+    let file_path = String::from("input_day1_1.txt");
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
